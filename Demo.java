b3bd32e32d2117728aaf0bfcc0224cacae7454df
Anurag Gupta <anuraggangil@gmail.com>
2024-11-27 22:25:07 +0530

    add something new

--- Demo.java
+++ Demo.java
@@ -2,5 +2,6 @@ public class Demo {
 
     public static void main(String[] args) {
         System.out.println("This is demo project of pipeline");
+        System.out.println("Add something new");
     }
 }
